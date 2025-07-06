d3de630810cd4559779cd631b1b0e7cc666364a1
rootvero <veblarv@gmail.com>
2025-07-06 19:15:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hola Mundo JavaScript</title>
+</head>
+<body>
 
+<script>
+  console.log("Hola Mundo desde la consola");
+  alert("Hola Mundo desde la alerta");
+  document.write("Hola Mundo desde document.write()");
+</script>
+
+</body>
+</html>
